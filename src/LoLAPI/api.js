63c061d2c4b853e44f00f1af444c3b5085e8422c
Rich <richcherng@gmail.com>
2018-08-29 21:35:28 -0700

    Clean up plus new API

--- src/LoLAPI/api.js
+++ src/LoLAPI/api.js
@@ -1,84 +1,100 @@
 
 /*
-	This file contain ONLY LoL API and does not contain any logic for gathering data
+  This file contain ONLY LoL API and does not contain any logic for gathering data
 
-	Every function here is a call to LoL API only and should not do any complicated logi
+  Every function here is a call to LoL API only and should not do any complicated logi
 */
-import fs from 'fs';
-import request from 'request'
-import polyfill from 'babel-polyfill'
+import fs from "fs";
+import request from "request"
+import polyfill from "babel-polyfill"
 
-let url_getStat = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"; // This need to be move out ot config file
-let url_matches = "https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/"
-let url_match = "https://na1.api.riotgames.com/lol/match/v3/timelines/by-match/"
+let URL_GETSTAT = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"; // This need to be move out ot config file
+let URL_MATCHEES = "https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/"
+let URL_MATCHE = "https://na1.api.riotgames.com/lol/match/v3/timelines/by-match/"
+let URL_CHAMP = "https://na1.api.riotgames.com//lol/platform/v3/champion-rotations/"
 
 
-let key = fs.readFileSync('./api_key');
+let key = fs.readFileSync("./api_key"); // Config file need to plan and implemented later
 
-key = '?api_key=' + key;
-console.log(key.toString());
+key = "?api_key=" + key;
 
 
 export let getSummonerStat = async (_summonerName) => {
 
-	try{
-		let stat = await doRequest(`${url_getStat}${_summonerName}/${key}`, 'GET');
-		return stat;
-	} catch(err) {
-		return err;
-	}
+  try{
+    let stat = await doRequest(`${URL_GETSTAT}${_summonerName}/${key}`, "GET");
+    return stat;
+  } catch(err) {
+    console.log(err.stack);
+    return err.stack;
+  }
 }
 
 export let getMatches = async (_accountID) => {
-	try{
-		let matches = await doRequest(`${url_matches}${_accountID}/${key}`, 'GET');
-		return matches;
-	} catch(err){
-		return err;
-	}
+  try{
+    let matches = await doRequest(`${URL_MATCHEES}${_accountID}/${key}`, "GET");
+    return matches;
+  } catch(err){
+    console.log(err.stack);
+    return err.stack;
+  }
 }
 
 
 export let getMatch = async (_matchID) => {
-	try{
-		let match = await doRequest(`${url_match}${_matchID}/${key}`, 'GET');
-		return match;
-	} catch(err){
-		return err;
-	}
+  try{
+    let match = await doRequest(`${URL_MATCHE}${_matchID}/${key}`, "GET");
+    return match;
+  } catch(err){
+    console.log(err.stack);
+    return err.stack;
+  }
+}
+
+export let getChamp = async () => {
+  try{
+    let champs = await doRequest(`${URL_CHAMP}${key}`, "GET");
+    return champs;
+  } catch (err) {
+    console.log(err.stack);
+    return err.stack;
+  }
 }
 
 
 function doRequest(_url, _method){
-	return new Promise( (resolve, reject) => {
-		try{
-			var options = {
-				url: _url,
-				method: _method,
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				json: true
-			};
+  console.log(_url);
+  return new Promise( (resolve, reject) => {
+    try{
+      var options = {
+        url: _url,
+        method: _method,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        json: true
+      };
 
-			request(options, (err, res, body)=>{
-				if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-					// console.log(' response ', res.body.rates.INR);
-					resolve(res.body);
-				} else {
-					console.log('error body', body);
-					reject(new Error(`Error body: ${JSON.stringify(body)}`));
-				}
-			});
-		} catch (err) {
-			console.log(" error request ", err);
-			process.exit();
-		}
-	})
+      request(options, (err, res, body)=>{
+        if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+          // console.log(" response ", res.body.rates.INR);
+          resolve(res.body);
+        } else {
+          console.log("error body", body);
+          reject(new Error(`Error body: ${JSON.stringify(body)}`));
+        }
+      });
+    } catch (err) {
+      console.log(" error request ", err);
+      process.exit();
+    }
+  })
 }
 
+// Just in case i need to calculate circumference...
 const pi = 3.14159265359;
 export var calcCircumference = (radius) => {
     return 2 * radius * pi;
 };
 
+  
--- src/LoLAPI/summoner.js
+++ src/LoLAPI/summoner.js
@@ -1,24 +1,24 @@
 
 
 /*
-	This file contains logic that handling summoner information such as:
-		- Match History
-		- Live Game 
-		- Account information
+  This file contains logic that handling summoner information such as:
+    - Match History
+    - Live Game 
+    - Account information
 */
 
 import {getSummonerStat, getMatches} from './api.js'
 // import polyfill from 'babel-polyfill'
 
 export let getSummoner = async (_summoner) => {
-	let info = await getSummonerStat(_summoner);
+  let info = await getSummonerStat(_summoner);
 
-	let match_history = await getMatches(info.accountId);
+  let match_history = await getMatches(info.accountId);
 
-	let stat = {
-		info: info,
-		matches: match_history
-	};
+  let stat = {
+    info: info,
+    matches: match_history
+  };
 
-	return stat;
+  return stat;
 }

6b43c4b4b3097252b4538cbde1d44059bdae31dd
rich <richcherng@gmail.com>
2018-11-03 00:56:38 -0700

    Docker-compose for deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // const express = require("express");
 // const app = express();
 
+import polyfill from 'babel-polyfill'
 import express from "express";
 import path from 'path';
 import { getMatch } from "./LoLAPI/api.js"; 

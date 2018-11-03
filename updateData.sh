f49da1771130906dc5b3c6ff57a428429ba60450
rich <richcherng@gmail.com>
2018-11-02 22:04:01 -0700

    Update Static Content

---
+++ updateData.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Get content version status
+curl https://ddragon.leagueoflegends.com/realms/na.json > response
+
+WORKDIR=${PWD}/data
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+
+# Parse Version from the response
+python << EOF > version
+
+import json 
+
+with open('response') as f:
+  data = json.load(f)
+
+  print data["dd"]
+
+EOF
+
+
+VERSION=$(cat version)
+
+
+curl https://ddragon.leagueoflegends.com/cdn/dragontail-$VERSION.tgz > $WORKDIR/data.tgz
+
+tar -xvzf $WORKDIR/data.tgz -C $WORKDIR/
+
+
+# Remove unused files
+rm version
+rm response
+rm data/data.tgz
+

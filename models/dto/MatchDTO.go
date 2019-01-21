371523569fcbfde885eb88d08dc6fbcf84339cfc
Michael Handria <michaelhandria@gmail.com>
2019-01-21 08:07:26 -0800

    [fix] fixed the model for matchdto

--- models/dto/MatchDTO.go
+++ models/dto/MatchDTO.go
@@ -1,14 +1,12 @@
 package dto
 
 type MatchDTO struct {
-	GameID       int64  `json:"gameId"`
-	PlatformID   string `json:"platformId"`
-	GameCreation int64  `json:"gameCreation"`
-	GameDuration int64  `json:"gameDuration"`
-	QueueID      int    `json:"queueId"`
-	MapID        int    `json:"mapId"`
-	SeasonID     int    `json:"seasonId"`
-	GameVersion  string `json:"gameVersion"`
-	GameMode     string `json:"gameMode"`
-	GameType     string `json:"gameType"`
+	SeasonID              int                      `json:"seasonId"`
+	QueueID               int                      `json:"queueId"`
+	GameID                int64                    `json:"gameId"`
+	ParticipantIdentities []ParticipantIdentityDTO `json:"participantIdentities"`
+	GameVersion           string                   `json:"gameVersion"`
+	PlatformID            string                   `json:"platformId"`
+	GameMode              string                   `json:"gameMode"`
+	MapID                 int                      `json:"mapId"`
 }
